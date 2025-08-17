f3476f4a74ad40b145f43e6968506f196555b4de
BrandonVLA <bbarrantescalderon@vlacademy.com>
2025-08-16 19:29:48 -0600

    Code formatted

--- projects/todo-list/app.js
+++ projects/todo-list/app.js
@@ -4,23 +4,24 @@ const taskList = document.getElementById("task-list");
 
 //eventlistener for the add-btn
 addBtn.addEventListener("click", () => {
-    taskReceived = input.value.trim();
-    if(input.value.trim() !== "") { //if entered input is different than empty to capture the value
-        console.log("Captured Text:",taskReceived);
+  taskReceived = input.value.trim();
+  if (input.value.trim() !== "") {
+    //if entered input is different than empty to capture the value
+    console.log("Captured Text:", taskReceived);
 
-        const newTask =document.createElement("li"); 
-        newTask.textContent = taskReceived;
-        taskList.appendChild(newTask);
-        input.value = "";
-        input.style.order = "";
-    }else {
-        input.style.border = "2px solid red";
-        return;
-    }
+    const newTask = document.createElement("li");
+    newTask.textContent = taskReceived;
+    taskList.appendChild(newTask);
+    input.value = "";
+    input.style.order = "";
+  } else {
+    input.style.border = "2px solid red";
+    return;
+  }
 });
 
-taskList.addEventListener('dblclick', (event) => {
-    if(event.target.tagName === "LI") {
-        event.target.remove();
-    }
+taskList.addEventListener("dblclick", (event) => {
+  if (event.target.tagName === "LI") {
+    event.target.remove();
+  }
 });
