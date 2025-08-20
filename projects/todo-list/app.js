6e6d51bbcf06dfbfcb8f1ac894257640d4bfe9ce
BrandonVLA <bbarrantescalderon@vlacademy.com>
2025-08-19 21:41:32 -0600

    Added localStorage functions to the To-Do List

--- projects/todo-list/app.js
+++ projects/todo-list/app.js
@@ -12,6 +12,7 @@ addBtn.addEventListener("click", () => {
     const newTask = document.createElement("li");
     newTask.textContent = taskReceived;
     taskList.appendChild(newTask);
+    saveTasks();//added saveTasks so each time I add a new task then save remaining tasks
     input.value = "";
     input.style.order = "";
   } else {
@@ -23,5 +24,44 @@ addBtn.addEventListener("click", () => {
 taskList.addEventListener("dblclick", (event) => {
   if (event.target.tagName === "LI") {
     event.target.remove();
+    saveTasks(); //added saveTasks so each time I delete a task then save remaining tasks
   }
 });
+
+//localStorage SaveTasks Function
+
+function saveTasks() {
+  //selects all elements(li)  from the taskList
+  const tasks = taskList.querySelectorAll('li');
+  
+  //Creates an array with the with each task text
+  const tasksArray = [];
+  //searches on 'tasks' array for all the 'li' tags and for each task text push it to 'taskArray'
+  tasks.forEach(task => {
+    tasksArray.push(task.textContent);
+  });
+
+  //Converts the array tasks into a JSON String and saves it
+  localStorage.setItem('tasks', JSON.stringify(tasksArray));
+}
+
+//localStorage LoadTasks Function
+
+function loadTasks() {
+  //This line will obtain saved tasks if created 
+  const savedTasks = localStorage.getItem('tasks');
+  //if saved tasks,then process them
+  if(savedTasks) {
+    const tasksArray = JSON.parse(savedTasks)
+  
+    // Creates <li></li> elements for each saved task
+    tasksArray.forEach(taskText => {
+      const newTask = document.createElement('li');
+      newTask.textContent = taskText;
+      taskList.appendChild(newTask);
+    });  
+  }
+}
+
+//Executes when page loads so it loads all the saved tasks
+document.addEventListener('DOMContentLoaded',loadTasks);
